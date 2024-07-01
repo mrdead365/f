6a094139893f8b99f6ddd9af68d00cf1a101718a
AhsanZubair1994 <167232948+AhsanZubair1994@users.noreply.github.com>
2024-07-01 23:17:51 +0500

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -248,18 +248,18 @@ document.querySelector('.whatsapp-icon').addEventListener('click', setWhatsAppLi
 
 
 
-const links = document.querySelectorAll('.nav-link');
+// const links = document.querySelectorAll('.nav-link');
 
-// Loop through each link
-links.forEach(link => {
+// // Loop through each link
+// links.forEach(link => {
 
-  link.addEventListener('click', function(event) {
-    console.log(event)
-    // Remove 'active' class from all links
-    links.forEach(l => l.classList.remove('active'));
+//   link.addEventListener('click', function(event) {
+//     console.log(event)
+//     // Remove 'active' class from all links
+//     links.forEach(l => l.classList.remove('active'));
 
-    // Add 'active' class to the clicked link
-    this.classList.add('active');
-  });
-});
+//     // Add 'active' class to the clicked link
+//     this.classList.add('active');
+//   });
+// });
 
